827a1a03b199fe7cc46f610fd6498cee12fae484
gcarde01 <giocardenas2@icloud.com>
2019-11-09 16:24:04 +0000

    sextions

---
+++ javaScript/toggle.js
@@ -0,0 +1,27 @@
+function myFunction() {
+    var x = document.getElementById("burger");
+    // var y = document.getElementById("cat-section");
+    // var z = document.getElementById("value-b");
+    // var t = document.getElementById("fot");
+    
+
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+      z.style.display = "none";
+      t.style.display = "none";
+      
+    } else {
+      x.style.display = "none";
+    //   y.style.display = "block";
+    //   z.style.display = "block";
+    //   t.style.display = "block";
+    
+    
+    }
+ 
+
+
+
+        
+}
