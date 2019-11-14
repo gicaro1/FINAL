567854783ce7dceaba1be83cd70fa62f14e37657
gcarde01 <giocardenas2@icloud.com>
2019-11-14 10:29:36 +0000

    13-nov-2019

--- javaScript/toggle.js
+++ javaScript/toggle.js
@@ -1,8 +1,12 @@
-function myFunction() {
+function myFunction(x) {
+    x.classList.toggle("change");
     var x = document.getElementById("burger");
     var y = document.getElementById("cat-section");
     var z = document.getElementById("value-b");
     var t = document.getElementById("fot");
+    var h = document.getElementById("video-javaScript");
+   
+     
     
 
     if (x.style.display === "none") {
@@ -10,18 +14,14 @@ function myFunction() {
       y.style.display = "none";
       z.style.display = "none";
       t.style.display = "none";
+      h.style.display = "none";
+    
       
     } else {
       x.style.display = "none";
       y.style.display = "block";
       z.style.display = "block";
       t.style.display = "block";
-    
-    
-    }
- 
-
-
-
-        
+      h.style.display = "block";
+    }    
 }
