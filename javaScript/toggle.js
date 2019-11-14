690740b1dfddcd4c5a648fc4b4bc6ca2106e164b
gcarde01 <giocardenas2@icloud.com>
2019-11-14 17:36:39 +0000

    14-nov-2019 thursday

--- javaScript/toggle.js
+++ javaScript/toggle.js
@@ -1,27 +1,61 @@
 function myFunction(x) {
-    x.classList.toggle("change");
+  x.classList.toggle("change");
+}
+
+
+function myFunction(x) {
+    // x.classList.toggle("change");
     var x = document.getElementById("burger");
-    var y = document.getElementById("cat-section");
-    var z = document.getElementById("value-b");
-    var t = document.getElementById("fot");
-    var h = document.getElementById("video-javaScript");
-   
-     
+    // var y = document.getElementById("cat-section");
+    // var value = document.getElementById("value-b");
+    // var t = document.getElementById("fot");
     
+   
 
     if (x.style.display === "none") {
       x.style.display = "block";
-      y.style.display = "none";
-      z.style.display = "none";
-      t.style.display = "none";
-      h.style.display = "none";
-    
+      // y.style.display = "none";
+      // value.style.display = "none";
+      // t.style.display = "none";
+      // video.style.display = "none";
       
     } else {
       x.style.display = "none";
-      y.style.display = "block";
-      z.style.display = "block";
-      t.style.display = "block";
-      h.style.display = "block";
+      // y.style.display = "block";
+      // value.style.display = "block";
+      // t.style.display = "block";
+      // video.style.display = "block";
+     
     }    
 }
+
+
+  /* --JAVAS-SCRIPT books ] SECTION TAGGLE REMOVE  ALL THE SECTIONS WHEN THE TOGLLE IS TRIGGRED--*/
+
+// function myFunction(x) {
+//   // x.classList.toggle("change");
+//   var x = document.getElementById("burger");
+//   var y = document.getElementById("cat-section");
+//   var base = document.getElementById("base-book");
+//   var img = document.getElementById("image-book-cover");
+//   var t = document.getElementById("fot");
+
+
+
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//     y.style.display = "none";
+//     img.style.display = "none";
+//     base.style.display = "none";
+//     t.style.display = "none";
+    
+//   } else {
+//     x.style.display = "none";
+//     y.style.display = "block";
+//     img.style.display = "block";
+//     base.style.display = "block";
+//     t.style.display = "block";
+   
+   
+//   }    
+// }
