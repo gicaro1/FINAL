853b9bc76aebb290304e46b55d85182fca41cda0
gcarde01 <giocardenas2@icloud.com>
2019-11-21 23:05:35 +0000

    21-nov

--- javaScript/toggle.js
+++ javaScript/toggle.js
@@ -1,64 +1,14 @@
 function myFunction(x) {
   x.classList.toggle("change");
 }
-
-
 function myFunction(x) {
-    // x.classList.toggle("change");
+  
     var x = document.getElementById("burger");
-    // var y = document.getElementById("cat-section");
-    // var value = document.getElementById("value-b");
-    // var t = document.getElementById("fot");
-    // var dropMenue = document.getElementById("drop");
-    
-   
-
     if (x.style.display === "none") {
       x.style.display = "block";
-      // y.style.display = "none";
-      // value.style.display = "none";
-      // t.style.display = "none";
-      // video.style.display = "none";
-      // dropMenue.style.display = "none";
-      
     } else {
       x.style.display = "none";
-      // y.style.display = "block";
-      // value.style.display = "block";
-      // t.style.display = "block";
-      // video.style.display = "block";
-      // dropMenue.style.display = "block";
-     
+
     }    
 }
 
-
-  /* --JAVAS-SCRIPT books ] SECTION TAGGLE REMOVE  ALL THE SECTIONS WHEN THE TOGLLE IS TRIGGRED--*/
-
-// function myFunction(x) {
-//   // x.classList.toggle("change");
-//   var x = document.getElementById("burger");
-//   var y = document.getElementById("cat-section");
-//   var base = document.getElementById("base-book");
-//   var img = document.getElementById("image-book-cover");
-//   var t = document.getElementById("fot");
-
-
-
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-//     y.style.display = "none";
-//     img.style.display = "none";
-//     base.style.display = "none";
-//     t.style.display = "none";
-    
-//   } else {
-//     x.style.display = "none";
-//     y.style.display = "block";
-//     img.style.display = "block";
-//     base.style.display = "block";
-//     t.style.display = "block";
-   
-   
-//   }    
-// }
